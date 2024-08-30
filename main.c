0394df8a0f1397c7867279975ffd2902ea5da17b
Jeff Son <jeff@infograb.net>
2024-08-30 16:42:26 +0900

    Refactor main.c to improve code readability and consistency

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#pragma warning(disable:4996)
+#pragma warning(disable : 4996)
 
 #include <stdio.h>
 #include <conio.h>
@@ -6,24 +6,25 @@
 #include "card.h"
 #include "mylib.h"
 #include "game.h"
+#include "initialize.h"
 
 void remove_cursor()
 {
-	CONSOLE_CURSOR_INFO curInfo;
-	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
-	curInfo.bVisible = 0;
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+  CONSOLE_CURSOR_INFO curInfo;
+  GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+  curInfo.bVisible = 0;
+  SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 }
 
 int main()
 {
-	remove_cursor();
-	system("mode con cols=160 lines=40");
+  remove_cursor();
+  system("mode con cols=160 lines=40");
 
-	Init();
-	Game();
+  Init();
+  Game();
 
-	End();
-	
-	getch();
+  End();
+
+  getch();
 }
